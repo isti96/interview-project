8ab0562f14da0dd15abc376719ee1562f6730912
isti96 <isti.daroczi@gmail.com>
2026-01-13 17:26:26 +0200

    caaad

--- interview-project/Program.cs
+++ interview-project/Program.cs
@@ -21,7 +21,7 @@ builder.Services.AddControllersWithViews();
 //options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"), new MySqlServerVersion(new Version(8, 0, 33))));
+options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddAutoMapper(typeof(MappingProfile));
 
